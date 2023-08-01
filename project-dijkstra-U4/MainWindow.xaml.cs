95f5587fd50b7584fa637dff9efa24c03c572ac3
adrianvldz <valdezadriangildardo@gmail.com>
2023-08-01 18:19:52 -0500

    Leer archivo txt
    Se añadió la lectura de un archivo txt con ruta en el escritorio, al igual que el mapa genera el número de ciudades que tenga el archivo txt

--- project-dijkstra-U4/MainWindow.xaml.cs
+++ project-dijkstra-U4/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using project_dijkstra_U4.Folder;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace project_dijkstra_U4
 {
@@ -21,6 +23,8 @@ namespace project_dijkstra_U4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int citiesToAdd; // Declarar citiesToAdd como una variable miembro de la clase
+
         private const int MapRows = 20;
         private const int MapCols = 30;
 
@@ -46,11 +50,16 @@ namespace project_dijkstra_U4
             }
 
             CreateMap();
-            AddCities();
+            
         }
 
         private void CreateMap()
         {
+            gridMap.Children.Clear(); // Limpiamos el contenido existente antes de actualizar el mapa
+
+            gridMap.RowDefinitions.Clear(); // Limpiamos las definiciones de filas
+            gridMap.ColumnDefinitions.Clear(); // Limpiamos las definiciones de columnas
+
             for (int row = 0; row < MapRows; row++)
             {
                 gridMap.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
@@ -84,10 +93,97 @@ namespace project_dijkstra_U4
             }
         }
 
-        private void AddCities()
+
+        
+
+        private bool CityExistsAt(int row, int col)
+        {
+            foreach (var city in cities)
+            {
+                if (city.Row == row && city.Column == col)
+                    return true;
+            }
+            return false;
+        }
+
+        private void BTN_Leer_Click(object sender, RoutedEventArgs e)
+        {
+            string nombreArchivo = TXT_Archivo.Text;
+            string escritorio = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string rutaArchivo = Path.Combine(escritorio, nombreArchivo);
+
+            try
+            {
+                if (!File.Exists(rutaArchivo))
+                {
+                    MessageBox.Show("El archivo no existe en la ruta especificada.");
+                    return;
+                }
+
+                StreamReader lector = new StreamReader(rutaArchivo);
+
+                string linea = lector.ReadLine();
+
+                if (linea == null)
+                {
+                    MessageBox.Show("El archivo está vacío.");
+                    lector.Close();
+                    return;
+                }
+
+                if (int.TryParse(linea, out citiesToAdd)) //Pasamos el valor de la linea a la variable citiesToAdd
+                {
+                    CreateMap();
+
+                    while (!lector.EndOfStream)
+                    {
+                        linea = lector.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(linea)) // Verificar que la línea no esté vacía ni sea nula
+                        {
+                            string[] datos = linea.Split(' ');
+                            if (datos.Length >= 3) // Verificar que la línea contenga al menos tres elementos
+                            {
+                                string ciudad1 = datos[0];
+                                string ciudad2 = datos[1];
+                                int distancia;
+
+                                if (int.TryParse(datos[2], out distancia))
+                                {
+                                    //lógica para crear conexiones entre las ciudades basadas en las distancias
+
+                                    // Luego, si es posible, agrega las ciudades al mapa
+                                    AddCityToMap(ciudad1);
+                                    AddCityToMap(ciudad2);
+                                }
+                                else
+                                {
+                                    MessageBox.Show($"Error al leer la distancia en la línea: {linea}");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Formato incorrecto en la línea: {linea}");
+                            }
+                        }
+                    }
+
+                    lector.Close();
+                }
+                else
+                {
+                    MessageBox.Show("El archivo no tiene un formato válido para el número de ciudades.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al intentar leer el archivo: " + ex.Message);
+            }
+        }
+
+
+        private void AddCityToMap(string cityName)
         {
             Random random = new Random();
-            int citiesToAdd = 10; // Número total de ciudades (se va a tener que cambiar por lo del txt)
 
             while (cities.Count < citiesToAdd)
             {
@@ -96,7 +192,7 @@ namespace project_dijkstra_U4
 
                 if (map[row, col] == 0 && !CityExistsAt(row, col))
                 {
-                    City city = new City("Ciudad " + (cities.Count + 1), row, col);
+                    City city = new City(cityName, row, col);
                     cities.Add(city);
 
                     Ellipse cityEllipse = new Ellipse
@@ -110,18 +206,11 @@ namespace project_dijkstra_U4
                     Grid.SetColumn(cityEllipse, col);
                     gridMap.Children.Add(cityEllipse);
                     cityEllipses[row, col] = cityEllipse;
+                    break;
                 }
             }
         }
 
-        private bool CityExistsAt(int row, int col)
-        {
-            foreach (var city in cities)
-            {
-                if (city.Row == row && city.Column == col)
-                    return true;
-            }
-            return false;
-        }
+
     }
 }
